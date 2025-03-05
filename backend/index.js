0a8a33cc4f4f3d17906bc1f5cdd46cfd732d3f5f
Aryan Chanana <142363827+Aryanthecoder07@users.noreply.github.com>
2025-03-05 21:46:32 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -70,5 +70,5 @@ app.post("/get-track", async (req, res) => {
         return res.status(500).json({ message: "Server error" });
     }
 });
-
-app.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
